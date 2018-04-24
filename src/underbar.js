62f5ba50334d90f3b3e62018829be0900aa59e14
grantyang <grantyang1@gmail.com>
2018-04-24 16:28:00 -0700

    underbar part 2 up until memoize complete

--- src/underbar.js
+++ src/underbar.js
@@ -171,32 +171,22 @@
   _.reduce = function(collection, iterator, accumulator) {
     if (accumulator === undefined) {
       accumulator = collection[0];
-      
-      for (var i = 1; i < collection.length; i++) {
-        // console.log(accumulator)
-        accumulator = iterator(accumulator, collection[i]);
-      }
+      _.each(collection.slice(1), function(item, index) {
+        accumulator = iterator(accumulator, item);
+      });  
     } else {
-      for (var i = 0; i < collection.length; i++) {
-
-        accumulator = iterator(accumulator, collection[i]);
-      }
+      _.each(collection, function(item, index) {
+        accumulator = iterator(accumulator, item);
+      });  
     }
-    
-    // _.each(collection, function(item, index){
-    //   accumulator += iterator(item);
-    // });
-    
     return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
   _.contains = function(collection, target) {
-    console.log(collection);
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
     return _.reduce(collection, function(wasFound, item) {
-      console.log(item);
       if (wasFound) {
         return true;
       }
@@ -207,18 +197,25 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
-    return _.reduce(collection, function(wasFound, item) {
-      if (!wasFound) {
+    iterator ? iterator : iterator = _.identity;
+    return _.reduce(collection, function(falseWasFound, item) {
+      if (!iterator(item)) {
         return false;
-      }
-      return item === iterator(item);
+      } 
+      return falseWasFound;
     }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
+    iterator ? iterator : iterator = _.identity;
+    return _.reduce(collection, function(wasFound, item) {
+      if (iterator(item)) {
+        return true;
+      } 
+      return wasFound;
+    }, false);
   };
 
 
@@ -241,11 +238,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var newArgs = [...arguments];
+    for (var arg of newArgs) {
+      Object.assign(obj, arg);
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var newArgs = [...arguments];
+    for (var arg of newArgs) {
+      for (var key in arg) {
+        if (obj[key] === undefined) {
+          obj[key] = arg[key];
+        }
+      }
+    }
+    return obj;
   };
 
 
